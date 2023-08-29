10eeed786764cc7fad6633a2e575d5ffefd5154e
jamyth.luk <jamyth.luk@okg.com>
2023-08-29 19:34:04 +0800

    [FEATURE]: jed-nvim: added lsp

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,19 @@
+print('Loading LSP')
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.ensure_installed({
+	'tsserver',
+	'eslint',
+	'rust_analyzer'
+})
+
+lsp.setup()
