3ceb11fafa5d0eddf751b05f7e056cab62799052
Jamyth <jamyth0712@gmail.com>
2023-08-31 00:16:33 +0800

    [REFACTOR]: jed-nvim: update lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -19,7 +19,6 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
   vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set('n', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
-  
 end)
 
 lsp.ensure_installed({
